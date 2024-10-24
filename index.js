c8ee56a8cae9afdc2c752049ca7e59db59065c2b
Osman Pérez <operez@edutecno.com>
2024-10-24 19:49:59 -0400

    API con listar, modificar y eliminar

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const { readFileSync, writeFileSync } = require("fs")
 const port = 3000;
 
-
+// Ubicación del archivo de persistencia.
 const dataAnimales = `${__dirname}/data/animales.txt`
 
 http.createServer((req, res) => {
@@ -16,8 +16,27 @@ http.createServer((req, res) => {
 
     if(pathName == "/animales") {
         if(metodo == "GET") {
-            const params = urlParsed.query
-            res.end("Listado de animales")
+            res.setHeader("Content-Type","application/json");
+            const params = urlParsed.query;
+            // console.log(params);
+            const contentString = readFileSync(dataAnimales, "utf-8");
+            let contentJS = JSON.parse(contentString);
+            contentJS = contentJS.filter( animal => {
+                if(params.habitat && animal.habitat.toUpperCase() == params.habitat.toUpperCase()) {
+                    return true
+                }
+
+                if(params.especie && animal.especie.toUpperCase() == params.especie.toUpperCase()) {
+                    return true
+                }
+
+                if(!params.habitat && !params.especie) {
+                    return true
+                }
+
+                return false
+            })
+            res.end(JSON.stringify({ message: "Listado de animales", data: contentJS }))
         } else if(metodo == "POST") {
             let body="";
             req.on("data", (chunk) => {
@@ -46,9 +65,38 @@ http.createServer((req, res) => {
             })
             
         } else if(metodo == "PUT") {
-            res.end("Editar de animales")
+            res.setHeader("Content-Type", "application/json");
+            let body = "";
+            req.on("data", (parte) => {
+                body += parte.toString()
+            })
+
+            req.on("end", () => {
+                body = JSON.parse(body);
+                console.log(body);
+                const contentString = readFileSync(dataAnimales, "utf-8");
+                let contentJS = JSON.parse(contentString);
+
+                let busqueda = contentJS.findIndex(animal => animal.id == body.id)
+                if(busqueda != -1) {
+                    contentJS[busqueda] = { ...contentJS[busqueda], ...body }
+                    writeFileSync(dataAnimales, JSON.stringify(contentJS), "utf-8")
+                    res.end(JSON.stringify({ message: "Animal modificado con éxito", data: contentJS[busqueda]}))
+                }  
+            })
+            
         } else if(metodo == "DELETE") {
-            res.end("Eliminar de animales")
+            res.setHeader("Content-Type", "application/json");
+            const params = urlParsed.query
+            const contentString = readFileSync(dataAnimales, "utf-8");
+            let contentJS = JSON.parse(contentString);
+
+            let busqueda = contentJS.findIndex(animal => animal.id == params.id)
+            if(busqueda != -1) {
+                const eliminado = contentJS.splice(busqueda, 1)
+                writeFileSync(dataAnimales, JSON.stringify(contentJS), "utf-8")
+                res.end(JSON.stringify({ message: "Animal eliminado con éxito", data: eliminado}))
+            }
         }
     }
 
